fea8b1aced0af4b05dd66abf1afb600f07115501
Pawel <pawel.klasa@gmail.com>
2017-10-26 21:18:17 +0100

    todo list set up with an array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var todos = [
+  {
+    todoTitle: 'My first todo',
+    todoResponsible: 'Pawel',
+    todoDescription: 'My first todo description',
+    todoPriority: 'low'
+  },
+  {
+    todoTitle: 'My second todo',
+    todoResponsible: 'Pawel',
+    todoDescription: 'My second todo description',
+    todoPriority: 'medium'
+  },
+  {
+    todoTitle: 'My third todo',
+    todoResponsible: 'Pawel',
+    todoDescription: 'My third todo description',
+    todoPriority: 'high'
+  }
+]
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h4>Todo count: <span className="badge badge-secondary">{this.state.todos.length}</span></h4>
+        <ul className="lost-group">
+          {this.state.todos.map((todo, index) =>
+            <li className="list-group-item" key={index}>
+              <h4 className="list-group-item-heading">{todo.todoTitle}</h4>
+            </li>
+          )}
+        </ul>
       </div>
+
     );
   }
 }
