90f278349fdb83a926ec67ad6621fd421d46b144
Pawel <pawel.klasa@gmail.com>
2017-10-27 21:23:39 +0100

    empty array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-var todos = [
-  {
-    todoTitle: 'My first todo',
-    todoResponsible: 'Sebastian',
-    todoDescription: 'Todo description',
-    todoPriority: 'low'
-  },
-  {
-    todoTitle: 'My second todo',
-    todoResponsible: 'Sebastian',
-    todoDescription: 'Todo description',
-    todoPriority: 'medium'
-  },
-  {
-    todoTitle: 'My third todo',
-    todoResponsible: 'Sebastian',
-    todoDescription: 'Todo description',
-    todoPriority: 'high'
-  }
-]
+var todos = []
 
 class App extends Component {
   constructor(props) {
